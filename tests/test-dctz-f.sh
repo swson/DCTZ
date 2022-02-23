7f5a4c0f1b26038ddd089fa41774055bcbe85c41
Cloud User <ec2-user@ip-172-31-7-58.ec2.internal>
2022-02-23 05:13:00 +0000

    add test script for floats

---
+++ tests/test-dctz-f.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "deleting *.z*, *.r, ..."
+rm -f *.z* *.r 
+
+#wget http://www.mcs.anl.gov/~shdi/download/CESM-ATM-tylor.tar.gz
+#tar xvfz CESM-ATM-tylor.tar.gz
+
+for i in '1E-3' '1E-4' '1E-5'
+do
+    for f in "CESM-ATM-tylor/1800x3600/CLDHGH_1_1800_3600.dat 3600 1800"
+    do
+	X=`echo $f|cut -c1-4`
+	echo running dctz-ec-test $f $i
+	../dctz-ec-test -f $i var $f 2>&1 | tee dctz-ec-comp.$X.$i.log
+	echo running dctz-qt-test $f $i
+	../dctz-qt-test -f $i var $f 2>&1 | tee dctz-qt-comp.$X.$i.log
+    done
+done
